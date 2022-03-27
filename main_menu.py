1a12cd7a5631a5e707b3baefc748e82b8b105563
bjornelvar <bjornet21@ru.is>
2022-03-26 23:34:43 -0700

    testing add to txt file

--- main_menu.py
+++ main_menu.py
@@ -10,25 +10,51 @@ def main_menu_print():
     print(main_menu)
 
 def main_menu_input():
-    main_menu_input = input("Please select an option: ")
-    if main_menu_input == "1":
+    main_menu_print()
+    menu_input = input("Please select an option: ")
+    if menu_input == "1":
         play_game()
-    elif main_menu_input == "2":
+    elif menu_input == "2":
         add_words()
-    else:
+    elif menu_input == "3":
         return
+    else:
+        print("Please enter a valid input")
+        main_menu_input()
 
 
 def play_game():
     print('playing game')
 
+def add_more_words():
+    prompt = input('Would you like to add another word? (y/n): ').lower()
+    if prompt == 'y':
+        add_words()
+    elif prompt == 'n':
+        main_menu_input()
+    else:
+        print('Please enter a valid input')
+        add_more_words()
+
 def add_words():
-    print('What words would you like to append to the word bank?')
+    print('What word would you like to append to the word bank?')
     word_bank_input = input('Input word: ')
-    file = open('test_words.txt', 'w')
-    file.write(word_bank_input)
+    file = open('test_words.txt', 'a')
+    if duplicate_check(word_bank_input):
+        file.write(word_bank_input + '\n')
+        print(f'"{word_bank_input}" added to word bank')
+        file.close() # Uppfærist með hverju instance-i. Annars var það bara þegar forritið hættir keyrslu
+        add_more_words()
+    else:
+        print('Word already in word bank')
+        add_more_words()
 
+def duplicate_check(word):
+    with open('test_words.txt', 'r') as file:
+        words = file.read()
+    if word not in words:
+        return True
+    else:
+        return False
 
-    
-main_menu_print()
 main_menu_input()
