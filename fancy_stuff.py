f8684237ce14e3235b3388a5566b3c180e53a4ea
bjornelvar <bjornet21@ru.is>
2022-03-28 11:38:30 -0700

    Setja fancy stuff i nýjan fæl

---
+++ fancy_stuff.py
@@ -0,0 +1,40 @@
+import time
+import sys
+import os
+from wordle_class_test import Wordle
+
+
+class FancyStuff:
+    def __init__(self):
+        self.header = """
+----------------------------------
+
+WELCOME TO WORDLE - PYTHON EDITION
+    Author - Jóhann Karlsson
+"""
+        self.menu_header = """
+----------------------------------------------------------
+
+                        MAIN MENU
+
+1. Play      2. Add Words      3. Edit Game       4. Quit
+
+----------------------------------------------------------
+"""
+    def load_wordle(self, profile):
+        '''ANIMATION FUNCTION'''
+        print('Logging in as ' + profile)
+        animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+        for i in range(len(animation)):
+            time.sleep(0.12)
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            sys.stdout.flush()
+        print("\n")
+        Wordle().play_wordle(profile)
+
+    def clear_console(self):
+        '''HELPER FUNCTION TO CLEAR SCREEN'''
+        command = 'clear'
+        if os.name in ('nt', 'dos'):
+            command = 'cls'
+        os.system(command)
