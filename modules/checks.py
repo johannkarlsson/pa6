74a10d04cc16ca60ad055bb208268b0a6d5c1ea3
bjornelvar <bjornet21@ru.is>
2022-03-30 10:53:46 -0700

    comments and docstrings

--- modules/checks.py
+++ modules/checks.py
@@ -1,6 +1,6 @@
 class Check:
     def duplicate_word_check(self, word_bank, word):
-        '''Checks if word already exists in wordbank'''
+        ''' When adding. Checks if word already exists in wordbank '''
         with open(word_bank, 'r') as file:
             words = file.read()
         if word not in words:
@@ -9,14 +9,14 @@ class Check:
             return False
 
     def add_word_length_check(self, word_length):
-        '''Checks if word_length is a digit between 4 and 6'''
+        ''' When adding. Checks if word_length is a digit between 4 and 6 '''
         if int(word_length) > 3 and int(word_length) < 7:
             return True
         else:
             return False
 
     def max_guesses_check(self, max_guesses):
-        '''Checks if max_guesses is a digit between 1 and 10'''
+        ''' When editing. Checks if max_guesses is a digit between 1 and 10 '''
         if max_guesses.isdigit():
             if int(max_guesses) > 0 and int(max_guesses) < 11:
                 return True
@@ -26,7 +26,7 @@ class Check:
             return False
     
     def word_letters_check(self, word_letters):
-        '''Checks if word_letters is a digit between 4 and 6'''
+        ''' When editing. Checks if word_letters is a digit between 4 and 6 '''
         if word_letters.isdigit():
             if int(word_letters) > 3 and int(word_letters) < 7:
                 return True
